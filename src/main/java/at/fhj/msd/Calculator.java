d9ae8ca5620eae8ee37ab5ea07fd6b430bb670c1
Marin Sekic <marin.sekic@edu.fh-joanneum.at>
2022-05-25 14:45:55 +0200

    javadoc comments added

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -3,18 +3,43 @@ package at.fhj.msd;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * @author marinsekic
+ */
+
 public class Calculator {
     private static Logger logger = LogManager.getLogger();
+
+    /**
+     * This method calculates the sum of two numbers.
+     * @param number1 the first number
+     * @param number2 the second number
+     * @return the sum of both numbers
+     */
 
     public double add(double number1, double number2) {
         logger.debug(number1 + number2);
         return number1 + number2;
     }
 
+    /**
+     * This method calculates the difference between two numbers.
+     * @param number1 the first number
+     * @param number2 the second number by which the first number is subtracted
+     * @return the difference between number1 and number2
+     */
+
     public double minus(double number1, double number2) {
         logger.debug(number1 + number2);
         return number1 - number2;
     }
+
+    /**
+     * This method divides two numbers and also does not allow a division by zero.
+     * @param number1 the first number of the division which is divided
+     * @param number2 the second number of the division with which is divided to
+     * @return the result of the division
+     */
 
     public double divide(double number1, double number2) {
         logger.debug(number1 + number2);
@@ -26,10 +51,23 @@ public class Calculator {
         }
     }
 
+    /**
+     * This method calculates the product of two numbers.
+     * @param number1 first number
+     * @param number2 second number
+     * @return product of both numbers
+     */
+
     public double multiply(double number1, double number2) {
         logger.debug(number1 + number2);
         return number1 * number2;
     }
+
+    /**
+     * This method calculates the faculty of one number
+     * @param number number of which the faculty is calculated
+     * @return faculty of the number
+     */
 
     public double faculty(int number) {
         if (number < 1) {
